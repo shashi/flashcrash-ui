fd0c88a882eaf893184366d996fd4b9e63fd19b0
Shashi Gowda <shashigowda91@gmail.com>
2015-07-08 21:05:22 +0530

    Add search box and category filter

--- index.jl
+++ index.jl
@@ -8,10 +8,9 @@ include(joinpath(pwd(), "input.jl"))
 include(joinpath(pwd(), "output.jl"))
 
 function main(window)
-    push!(window.assets, "date")
     vbox(
         h1("Julia Trade Data Viewer"),
         vskip(2em),
-        input_pane()
+        input_pane(window)
     ) |> pad(1em)
 end
--- input.jl
+++ input.jl
@@ -10,31 +10,52 @@ if !isdefined(Main, :_metadata_loaded)
     const _metadata_loaded = true
 end
 
-render(t::Escher.Clip, state) =
-    render(Escher.Container(t.tile), state) &
-        [:style => [:overflow => Escher.name(t.overflow)], :className => "scrollbar"]
-
 function ticker_list(selected, filter_categories, startstr, current_ticker)
     list = length(filter_categories) == 0 ?
-             tickers : gettickers(filter_categories)
+             tickers : gettickers([filter_categories...])
     list = startstr == "" ? list : filter(x -> beginswith(x, startstr), list)
 
     map(list) do t
-        item = (t == selected ? fillcolor("#9ba", t |> pad(0.5em)) : t |> pad(0.5em))
+        item = (t == selected ? fillcolor("#9ba", pad(0.5em, t)) : pad(0.5em, t))
         constant(t, clickable(item)) >>> current_ticker
-    end |> vbox |> clip(auto) |> size(10em, 25em)
+    end |> vbox |> clip(auto) |> size(10em, 20em) 
 end
 
-function input_pane()
+function category_list(selection_stream, selected)
+    map(categories) do c
+        hbox(pairwith(c, checkbox(c in selected)) >>> selection_stream, hskip(2em), c) |> pad(0.5em)
+    end |> vbox |> clip(auto) |> size(20em, 25em) 
+end
+
+function update_selection(prev, s)
+    added, category = s
+    if added
+        push!(prev, category)
+    else
+        pop!(prev, category)
+    end
+    return prev
+end
+
+function input_pane(window)
+    push!(window.assets, "date")
+    push!(window.assets, "widgets")
+
     current_date = Input(start_date)
     current_ticker = Input("A")
     search_string = Input("")
-    selected_categories = Input([])
+    selection_stream = Input{Any}((nothing, nothing))
+    selected_categories = foldl(update_selection, Set(String[]), selection_stream)
 
     hbox(
         datepicker(start_date, range=date_range) >>> current_date,
         hskip(1em),
-        lift((x, y, z) -> ticker_list(x, y, z, current_ticker),
-             current_ticker, selected_categories, search_string)
+        vbox(
+            textinput(label="Search", floatinglabel=false) >>> search_string,
+            lift((x, y, z) -> ticker_list(x, y, z, current_ticker),
+                 current_ticker, selected_categories, search_string)
+         ),
+         hskip(1em),
+         lift(cs -> category_list(selection_stream, cs), selected_categories)
     )
 end
